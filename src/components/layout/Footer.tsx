7acdff57e46d917beef6398bc3169b99de79d37a
mathlun <matheuslunadev@gmail.com>
2026-08-07 01:56:57 +0000

    feat(design-system): apply initial styling to landing page

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,6 @@
-
+import styles from './Footer.module.css';
 export const Footer = () => (
-	<footer className="footer">
-	  <button>
-	    Começar a ler
-	  </button>
-
+	<footer className={styles.container}>
+	 <p>&copy; 2026 - Matheus Luna</p>
 	</footer>
 );
